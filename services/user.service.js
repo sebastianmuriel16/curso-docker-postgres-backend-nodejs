75e97bf291310119cbb035b95f7e7866b79d2c65
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2024-04-05 17:04:05 -0500

    sequilize: orm y user.model implementados con crud

--- services/user.service.js
+++ services/user.service.js
@@ -1,30 +1,37 @@
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres.pool');
+
+const { models } = require('../libs/sequelize');
+
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const rta = await pool.query(query);
-    return rta.rows;
+    const rta = models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
